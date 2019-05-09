2b66706714547a798066f6920ac13278a974efd9
Joshua deLorimier <TRASHTRASH_HQ@Joshuas-Mac-Pro.localdomain>
2019-05-09 12:19:22 -0400

    finished

--- server.js
+++ server.js
@@ -1,17 +1,12 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-// const path = require('path');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-const todoRoutes = express.Router();
 const PORT = process.env.PORT || 4000;
-let Todo = require('./models/todosmodel');
 
 app.use(cors());
 app.use(bodyParser.json());
-// app.use(express.urlencoded({ extended: true }));
-// app.use(express.json());
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/todos', { useNewUrlParser: true });
 const connection = mongoose.connection;
@@ -20,55 +15,9 @@ connection.once('open', function() {
     console.log("MongoDB database connection established successfully");
 })
 
-todoRoutes.route('/').get(function(req, res) {
-  Todo.find(function(err, todos) {
-      if (err) {
-          console.log(err);
-      } else {
-          res.json(todos);
-      }
-  });
-});
+require('./routes/apiRoutes.js')(app);
 
-todoRoutes.route('/:id').get(function(req, res) {
-  let id = req.params.id;
-  Todo.findById(id, function(err, todo) {
-      res.json(todo);
-  });
-});
 
-todoRoutes.route('/add').post(function(req, res) {
-  let todo = new Todo(req.body);
-  todo.save()
-      .then(todo => {
-          res.status(200).json({'todo': 'todo added successfully'});
-      })
-      .catch(err => {
-          res.status(400).send('adding new todo failed');
-      });
-});
-
-todoRoutes.route('/update/:id').post(function(req, res) {
-  Todo.findById(req.params.id, function(err, todo) {
-      if (!todo)
-          res.status(404).send('data is not found');
-      else
-          todo.todo_description = req.body.todo_description;
-          todo.todo_responsible = req.body.todo_responsible;
-          todo.todo_priority = req.body.todo_priority;
-          todo.todo_completed = req.body.todo_completed;
-
-          todo.save().then(todo => {
-              res.json('Todo updated');
-          })
-          .catch(err => {
-              res.status(400).send("Update not possible");
-          });
-  });
-});
-
-app.use('/todos', todoRoutes);
-// Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
